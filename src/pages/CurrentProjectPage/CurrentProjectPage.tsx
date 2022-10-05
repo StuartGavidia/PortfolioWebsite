7197d3f44ec0896b5245399062d0c78475cbae35
StuartGavidia <stuarta0403@hotmail.com>
2022-10-04 23:34:59 -0700

    Create CurrentProject Page

--- src/pages/CurrentProjectPage/CurrentProjectPage.tsx
+++ src/pages/CurrentProjectPage/CurrentProjectPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import './CurrentProjectPage.css'
+//import { useParams } from 'react-router-dom'
 
 
 const CurrentProjectPage = () => {
+
+    //const {project} = useParams()
+
+
+
     return (
         <h1>Current Project Page</h1>
     )
