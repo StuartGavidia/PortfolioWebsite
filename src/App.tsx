607a95707ed2736093dd056c1e1bf0f143947438
StuartGavidia <stuarta0403@hotmail.com>
2022-10-04 23:33:35 -0700

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,36 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import ProjectPage from './pages/ProjectPage/ProjectPage'
+import HomePage from './pages/HomePage/HomePage'
+import CurrentProjectPage from './pages/CurrentProjectPage/CurrentProjectPage'
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ProjectPage />
+      <Router>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Switch>
+          <Route exact path="/home">
+            <HomePage />
+          </Route>
+          <Route exact path="/home/:project">
+            <CurrentProjectPage />
+          </Route>
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+/*
+"devDependencies": {
+        "@types/react-router-dom": "^5.3.3"
+      }
+*/
