b4dfd1de76a44eafbc0f52f70988de2ff3a8e9e1
StuartGavidia <stuarta0403@hotmail.com>
2022-10-05 11:06:10 -0700

    Outsource data

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,40 +3,9 @@ import './HomePage.css'
 import Project from '../../components/Project/Project'
 import Info from '../../components/Info/Info'
 import { projectInfo } from '../../interfaces'
+import { projects } from '../../data'
 
 const HomePage = () => {
-
-    const projects:projectInfo[] = [
-        {
-            info: "PLU Learn",
-            date: "March 2022",
-            technologies: "Javascript, CSS, HTML, React, Weather API, Netlify",
-            quickBio: "A web application for chemistry students at PLU. Letting them hone the skills taught in second semester chemistry.",
-            img_link:  "plulearn.png"
-        },
-        {
-            info: "Notedle",
-            date: "May 2022",
-            technologies: "Typescript, CSS, HTML, Tone.js, Snowpack, Firebase",
-            quickBio: "Inspired by wordle, a music based game where the user has to figure out the beats to the song!",
-            img_link:  "notedle.png"
-        },
-        {
-            info: "Solar System",
-            date: "July 2022",
-            technologies: "Typescript, HTML, CSS, Three.js",
-            quickBio: "Replicating our solar system by emulating gravity and the orbits of planets.",
-            img_link:  "solarsystem.png"
-        },
-        {
-            info: "Jammming",
-            date: "April 2022",
-            technologies: "Javascript, HTML, CSS, Spotify API",
-            quickBio: "Want to save a playlist onto your Spotify account? Jammming does just that (Codecademy Project)",
-            img_link:  "jammming.png"
-        }
-    ]
-
     return (
         <div className="home">
             <Info />
