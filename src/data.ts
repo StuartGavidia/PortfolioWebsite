bdf40bd40114f9d4bcc7189ec078f24853499dd6
StuartGavidia <stuarta0403@hotmail.com>
2022-10-07 19:04:26 -0700

    Edit data

--- src/data.ts
+++ src/data.ts
@@ -2,31 +2,58 @@ import { projectInfo } from './interfaces'
 
 export const projects:projectInfo[] = [
     {
-        info: "PLU Learn",
+        title: "PLU Learn",
         date: "March 2022",
         technologies: "Javascript, CSS, HTML, React, Weather API, Netlify",
         quickBio: "A web application for chemistry students at PLU. Letting them hone the skills taught in second semester chemistry.",
-        img_link:  "plulearn.png"
+        img_link:  "plulearn.png",
+        info: {
+            description1: "",
+            description2: ""
+        }
     },
     {
-        info: "Notedle",
+        title: "Notedle",
         date: "May 2022",
-        technologies: "Typescript, CSS, HTML, Tone.js, Snowpack, Firebase",
+        technologies: "Typescript, CSS, HTML, React, Tone.js, Snowpack, Firebase",
         quickBio: "Inspired by wordle, a music based game where the user has to figure out the beats to the song!",
-        img_link:  "notedle.png"
+        img_link:  "notedle.png",
+        info: {
+            description1: "Hello there",
+            description2: "Oh yeahh yeah",
+        }
     },
     {
-        info: "Solar System",
+        title: "Solar System",
         date: "July 2022",
         technologies: "Typescript, HTML, CSS, Three.js",
         quickBio: "Replicating our solar system by emulating gravity and the orbits of planets.",
-        img_link:  "solarsystem.png"
+        img_link:  "solarsystem.png",
+        info: {
+            description1: "",
+            description2: ""
+        }
     },
     {
-        info: "Jammming",
+        title: "Jammming",
         date: "April 2022",
-        technologies: "Javascript, HTML, CSS, Spotify API",
-        quickBio: "Want to save a playlist onto your Spotify account? Jammming does just that (Codecademy Project)",
-        img_link:  "jammming.png"
+        technologies: "Javascript, HTML, CSS, React, Spotify API",
+        quickBio: "Want to save a playlist onto your Spotify account? Jammming does just that (Codecademy Project).",
+        img_link:  "jammming.png",
+        info: {
+            description1: "",
+            description2: ""
+        }
+    },
+    {
+        title: "PLU Programming Party Website",
+        date: "September 2022 (Currently Building)",
+        technologies: "Typsecript, HTML, CSS, React",
+        quickBio: "Creating a website to showcase projects developed by students at Pacific Lutheran University.",
+        img_link: "programmingpartyweb.png",
+        info: {
+            description1: "",
+            description2: ""
+        }
     }
 ]
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,8 +1,13 @@
-export interface projectInfo{
-    info: string,
+export interface projectInfo {
+    title: string,
     date: string,
     technologies: string,
     quickBio: string,
-    img_link: string
+    img_link: string,
+    info: {
+        description1: string,
+        description2: string,
+        description3?: string
+    }
 }
 
